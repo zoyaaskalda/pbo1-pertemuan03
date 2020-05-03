03e6dc7ac8cd36723f1d475f23686f48d4465c11
zoyaaskalda <zoyaaskalda@gmail.com>
2020-05-03 12:16:25 +0800

    Percabangan If Else S2 out

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if (uangDiDompet < totalBelanja){
             System.out.println("Uang kurang, Kurangi Jajan Anda");
-    }else{
-            System.out.println("Uang cukup, Selamat Menikmati");
+    }else if (uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
+        }else{
+            System.out.println("Uang pas.. UwU..");
 
     }
 }
