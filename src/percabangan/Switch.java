e7ad09e4a571fa71e70d64f554724c428ee1b55b
zoyaaskalda <zoyaaskalda@gmail.com>
2020-05-03 12:27:07 +0800

    percabanganswitch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            default:
+                System.out.println("mahluk halus");
+                break;
+
+        }
+    }
+}
