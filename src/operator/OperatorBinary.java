c254420658a508c191e21445135a91a2db585040
zoyaaskalda <zoyaaskalda@gmail.com>
2020-05-03 11:32:12 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignement/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Zoya Askalda";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
